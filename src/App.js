3ec65cae5377846bea521a55833771fbf8df5018
deryksouza <kelleysonderyk@gmail.com>
2025-05-21 16:00:52 -0300

    Adicionado sistema de login com autenticação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
 import PanicButton from './components/PanicButton';
 import Monitor from './components/Monitor';
+
+function PrivateRoute({ children }) {
+  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+  return isAuthenticated ? children : <Navigate to="/" />;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<PanicButton />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/panic" element={
+          <PrivateRoute>
+            <PanicButton />
+          </PrivateRoute>
+        } />
         <Route path="/monitor" element={<Monitor />} />
       </Routes>
     </Router>
